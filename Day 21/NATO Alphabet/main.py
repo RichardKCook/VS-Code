acacc32b1c7b7bbafc183e53ceefafa0f4ae00ca
Richard Cook <RichardKCooK@me.com>
2023-01-08 10:46:01 -0600

    Added Some Exception Clauses

--- Day 21/NATO Alphabet/main.py
+++ Day 21/NATO Alphabet/main.py
@@ -5,9 +5,14 @@ bet = pandas.read_csv(
 
 nato_dict = {row.letter:row.code for index,row in bet.iterrows()}
 
+def main():
+    name = input("What's your name?: ").upper()
+    try:
+        conversion = [nato_dict[letter] for letter in name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        return main()
+    else:
+        print(conversion)
 
-name = input("What's your name?: ").upper()
-
-conv = [nato_dict[letter] for letter in name]
-
-print(conv)
+main()
